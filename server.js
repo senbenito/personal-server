863d0c968d37afb8e4cb8c684a68e54390958ff3
Shannon Rivers <shannon.rivers@gmail.com>
2018-02-01 11:53:53 -0700

    /decks res.send(seed Object)

--- server.js
+++ server.js
@@ -19,11 +19,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // --> https://senbenito-server.herokuapp.com/
 app.get('/decks', req, res, next)=>{
-  knex('/decks')
-  .select('*')
-  .then(data=>{
-    res.send(data);
-  });
+  const initialData = {
+    Addition: {
+      title: 'Addition',
+      questions: [
+        {
+          question: '4 + 5',
+          answer: 9
+        },
+        {
+          question: '6 + 2',
+          answer: 8
+        }
+      ]
+    },
+    Subtraction: {
+      title: 'Subtraction',
+      questions: [
+        {
+          question: '9 - 5',
+          answer: 4
+        },
+        {
+          question: '8 - 2',
+          answer: 6
+        }
+      ]
+    }
+  };
+  res.send(initialData);
+  
+  // TODO: break initialData into relational tables
+  // knex('/decks')
+  // .select('*')
+  // .then(data=>{
+  //   res.send(data);
+  // });
 };
 
 app.get('/sites', (req,res,next)=>{
